2fa58b136eecdc80ac4fd644e92f098bd9bd82df
Pedro <pgs_cf@hotmail.com>
2022-05-30 21:28:39 -0300

    inclusão do vuex na aplicação

--- discografia-front/src/store/index.js
+++ discografia-front/src/store/index.js
@@ -1,17 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
+import Discography from './modules/discography';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  modules:{
+    Discography,
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  plugins:[createPersistedState()]
 })
---
+++ discografia-front/src/store/modules/discography.js
@@ -0,0 +1,43 @@
+const state = {//ou venda
+    albums:[{
+        tracks:[],
+    }],
+    artist:{
+        name:'',
+    }
+}
+
+const actions = { 
+    getAlbums({commit}){
+        //update to use AXIOS,
+        return new Promise(()=>{
+            var myHeaders = new Headers();
+                myHeaders.append("Content-Type", "application/json");
+                myHeaders.append("Accept", "application/json");
+            var requestOptions = {
+                method: 'GET',
+                headers: myHeaders,
+                redirect: 'follow'
+                };
+            fetch(process.env.BACK_URL+"albums", requestOptions)
+            .then(response => response.text())
+            .then(result =>{
+                result = JSON.parse(result);
+                commit('getAlbums', result);
+            })
+            .catch(error => console.log('error', error));
+        })
+    }
+}
+
+const mutations = {
+    getAlbums(state,result){
+        console.log(result);
+        state.albuns = result;
+    }
+}
+export default {
+    state,
+    actions,
+    mutations,
+  };
